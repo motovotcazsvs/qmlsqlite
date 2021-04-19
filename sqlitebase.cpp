b62f6f2aa5e0456415b31db8c05f0f99fe1bbaba
motovotcazsvs <78682549+motovotcazsvs@users.noreply.github.com>
2021-04-19 17:19:01 +0300

    dkjg

--- sqlitebase.cpp
+++ sqlitebase.cpp
@@ -19,10 +19,16 @@ void sqlitebase::pushRecord()
 {
     query.prepare("INSERT INTO TABLEMEDICALMEASUREMENTS(NUMBERPERDAY, PROBENUMBER, DATEYEAR, DATEMONTH, DATEDAY, TIMEHOUR, TIMEMINUTE, GL, LC, BARCODE)"
                   "VALUES(:number_per_day, :probe_number, :date_year, :date_month, :date_day, time_hour, time_minute, gl, lc, bar_code);");
-    query.bindValue(":imageData1", inByteArray1);
-    query.bindValue(":imageData2", inByteArray2);
-    //query.bindValue(":hesh", "0");
-    query.bindValue(":precent", "19");
+    query.bindValue(":NUMBERPERDAY", "10");
+    query.bindValue(":PROBENUMBER", "4");
+    query.bindValue(":date_year", "2021");
+    query.bindValue(":date_month", "4");
+    query.bindValue(":date_day", "19");
+    query.bindValue(":time_hour", "17");
+    query.bindValue(":time_minute", "9");
+    query.bindValue(":gl", "9.91");
+    query.bindValue(":lc", "3.88");
+    query.bindValue(":bar_code", "444444444");
     if(!query.exec()) qDebug() << "Error inserting filds into table" << query.lastError();
 }
 
